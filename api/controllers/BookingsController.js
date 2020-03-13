ea104f77c5cb560af912bfc7d25b77154bc1bbf1
cronycles <cronycles@gmail.com>
2020-03-13 18:54:44 +0100

    refactoring test

--- api/controllers/BookingsController.js
+++ api/controllers/BookingsController.js
@@ -2,17 +2,13 @@ export default class BookingsController {
 
   createABooking = (req, res) => {
     var booking = req.body;
-    if (booking.clientId) {
-      res.status(200).json({
-        booking: {}
-      });
-    } else {
+    if (!booking.clientId) {
       res.status(200).json({
         error: {
           id: 1,
           message: "Invalid client id"
         }
       });
-    }
+    } 
   };
 }
--- test/bookingsCoontroller.test.js
+++ test/bookingsCoontroller.test.js
@@ -1,22 +1,25 @@
 import app from "../server.js";
 import request from 'supertest';
-import {expect} from "chai"
+import { expect } from "chai"
 
 describe('BookingsController Test Suite', () => {
-  describe('POST /bookings with null clientId', () => {
-    it('responds error with id 1 and "Invalid client id "text', (done) => {
-      request(app)
-        .post('/bookings')
-        .send({ clientId: null })
-        .set('Accept', 'application/json')
-        .end((err, res) => { 
-          expect('Content-Type', /json/)
-          expect(res.statusCode).to.equal(200); 
-          expect(res.body.error.id).to.equal(1); 
-          expect(res.body.error.message).to.equal("Invalid client id"); 
-          expect(res.body.booking).to.be.undefined; 
-          done(); 
-        });
+  const INVALID_CLIENT_IDS = [null, undefined, 0];
+  INVALID_CLIENT_IDS.forEach(function (clientId) {
+    describe(`POST /bookings with invalid clientId (${clientId})`, () => {
+      it('responds error with id 1 and "Invalid client id "text', (done) => {
+        request(app)
+          .post('/bookings')
+          .send({ clientId: clientId })
+          .set('Accept', 'application/json')
+          .end((err, res) => {
+            expect('Content-Type', /json/)
+            expect(res.statusCode).to.equal(200);
+            expect(res.body.error.id).to.equal(1);
+            expect(res.body.error.message).to.equal("Invalid client id");
+            expect(res.body.booking).to.be.undefined;
+            done();
+          });
+      });
     });
   });
 });
